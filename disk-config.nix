34c1988043909f80852c582586a1cd408c2e907f
Eric Ahlberg <eahlberg@gmail.com>
2023-08-13 10:11:34 +0200

    Update config to make the vm-test run

--- disk-config.nix
+++ disk-config.nix
@@ -5,67 +5,20 @@
     type = "disk";
     content = {
       type = "gpt";
-      format = "gpt";
-      partitions = [
-        {
-          name = "boot";
-          start = "0";
-          end = "1M";
-          part-type = "primary";
-          flags = ["bios_grub"];
-        }
-        {
-          name = "ESP";
-          start = "1MiB";
-          end = "100MiB";
-          bootable = true;
-          content = {
-            type = "mdraid";
-            name = "boot";
-          };
-        }
-        {
-          name = "root";
-          start = "100MiB";
-          end = "100%";
-          part-type = "primary";
-          bootable = true;
-          content = {
-            type = "lvm_pv";
-            vg = "pool";
-          };
-        }
-      ];
-    };
-  });
-  mdadm = {
-    boot = {
-      type = "mdadm";
-      level = 1;
-      metadata = "1.0";
-      content = {
-        type = "filesystem";
-        format = "vfat";
-        mountpoint = "/boot";
-      };
-    };
-  };
-  lvm_vg = {
-    pool = {
-      type = "lvm_vg";
-      lvs = {
+      partitions = {
+        boot = {
+          size = "1M";
+          type = "EF02";
+        };
         root = {
-          size = "100%FREE";
+          size = "100%";
           content = {
             type = "filesystem";
             format = "ext4";
             mountpoint = "/";
-            mountOptions = [
-              "defaults"
-            ];
           };
         };
       };
     };
-  };
+  });
 }
